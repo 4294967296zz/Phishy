0a760a3a7aef1caa485585d9d9a4ec4015d74d5e
jyoghh <4294967296z@gmail.com>
2021-07-15 18:08:40 +0900

    20210715

--- src/main/java/phishy/controller/UserController.java
+++ src/main/java/phishy/controller/UserController.java
@@ -1,5 +1,6 @@
 package phishy.controller;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -9,6 +10,8 @@ import phishy.service.UserService;
 import phishy.util.ExcelRead;
 import phishy.util.ExcelReadOption;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -17,23 +20,24 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
+@AllArgsConstructor
 public class UserController {
 
     private UserService userService;
 
-    @RequestMapping(value = "/registerUser.do", method = RequestMethod.GET)
-    public String register(UserDto userDto,
-                           @RequestParam("user_id") String user_id,
-                           @RequestParam("user_email") String user_email,
-                           @RequestParam("user_nm") String user_nm,
-                           @RequestParam("user_pwd") String user_pwd,
-                           @RequestParam("user_rank") String user_rank,
-                           @RequestParam("corp_cd") String corp_cd,
-                           @RequestParam("corp_nm") String corp_nm,
-                           @RequestParam("org_cd") String org_cd,
-                           @RequestParam("org_nm") String org_nm,
-                           @RequestParam("level_gp") String level_gp,
-                           @RequestParam("level_lv") String level_lv) {
+    @PostMapping(value = "/registerUser")
+    public @ResponseBody Object register(
+            @RequestParam("user_id") String user_id,
+            @RequestParam("user_email") String user_email,
+            @RequestParam("user_nm") String user_nm,
+            @RequestParam("user_pwd") String user_pwd,
+            @RequestParam("user_rank") String user_rank,
+            @RequestParam("corp_cd") String corp_cd,
+            @RequestParam("corp_nm") String corp_nm,
+            @RequestParam("org_cd") String org_cd,
+            @RequestParam("org_nm") String org_nm,
+            @RequestParam("level_gp") String level_gp,
+            @RequestParam("level_lv") String level_lv) {
         Map<String, String> list = new HashMap<String, String>();
         list.put("user_id",user_id);
         list.put("user_email",user_email);
@@ -47,11 +51,42 @@ public class UserController {
         list.put("level_gp",level_gp);
         list.put("level_lv",level_lv);
         userService.registerUser(list);
-        return "redirect:/register?done=1";
 
+        return list;
     }
 
-    @RequestMapping(value = "/excelUpload.do", method = RequestMethod.POST)
+    @PostMapping(value = "/updateUser")
+    public @ResponseBody Object update(
+            @RequestParam("uid") Long uid,
+            @RequestParam("user_id") String user_id,
+            @RequestParam("user_email") String user_email,
+            @RequestParam("user_nm") String user_nm,
+            @RequestParam("user_pwd") String user_pwd,
+            @RequestParam("user_rank") String user_rank,
+            @RequestParam("corp_cd") String corp_cd,
+            @RequestParam("corp_nm") String corp_nm,
+            @RequestParam("org_cd") String org_cd,
+            @RequestParam("org_nm") String org_nm,
+            @RequestParam("level_gp") String level_gp,
+            @RequestParam("level_lv") String level_lv) {
+        Map<String, String> list = new HashMap<String, String>();
+        list.put("user_id",user_id);
+        list.put("user_email",user_email);
+        list.put("user_nm",user_nm);
+        list.put("user_pwd",user_pwd);
+        list.put("user_rank",user_rank);
+        list.put("corp_cd",corp_cd);
+        list.put("corp_nm",corp_nm);
+        list.put("org_cd",org_cd);
+        list.put("org_nm",org_nm);
+        list.put("level_gp",level_gp);
+        list.put("level_lv",level_lv);
+        userService.updateUser(uid, list);
+
+        return list;
+    }
+
+    @RequestMapping(value = "/excelUpload", method = RequestMethod.POST)
     public @ResponseBody
     Object excelUploadAjax(MultipartHttpServletRequest request)  throws Exception{
         MultipartFile excelFile =request.getFile("excelFile");
@@ -93,5 +128,29 @@ public class UserController {
         Object result = totallist;
         return result;
     }
+
+    @RequestMapping(value = "/getUserDatatable", method = RequestMethod.POST)
+    public @ResponseBody Object getDatatable(HttpServletRequest request,
+                                             HttpServletResponse response) {
+        Map<String, Object> mp = new HashMap<String, Object>();
+        mp.put("data", userService.getUserlist());
+        Object result = mp;
+        return result;
+    }
+
+    @RequestMapping(value = "/getUser", method = RequestMethod.POST)
+    public @ResponseBody Object getUser(@RequestParam("uid") Long uid) {
+        Map<String, Object> mp = new HashMap<String, Object>();
+        mp.put("data", userService.getUser(uid));
+        Object result = mp;
+        return mp;
+    }
+
+    @RequestMapping(value = "/deleteuser", method = RequestMethod.POST)
+    public String deleteuser(@RequestParam("uid") Long uid) {
+        userService.deleteUser(uid);
+        return "redirect:/user";
+    }
+
 
 }
--- src/main/java/phishy/service/UserService.java
+++ src/main/java/phishy/service/UserService.java
@@ -8,8 +8,10 @@ import phishy.domain.Repository.UserRepository;
 import phishy.dto.UserDto;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @AllArgsConstructor
 @Service
@@ -59,6 +61,73 @@ public class UserService {
 
             userRepository.save(userEntity).getUid();
         }
+    }
+
+    @Transactional
+    public List<UserDto> getUserlist() {
+        List<UserEntity> userEntities = userRepository.findAll();
+        List<UserDto> userDtoList = new ArrayList<>();
+
+        for(UserEntity userEntity : userEntities) {
+            UserDto userDTO = UserDto.builder()
+                    .uid(userEntity.getUid())
+                    .user_nm(userEntity.getUser_nm())
+                    .user_email(userEntity.getUser_email())
+                    .user_rank(userEntity.getUser_rank())
+                    .corp_nm(userEntity.getCorp_nm())
+                    .org_nm(userEntity.getOrg_nm())
+                    .build();
+
+            userDtoList.add(userDTO);
+        }
+        return userDtoList;
+    }
+
+    @Transactional
+    public void updateUser(Long uid, Map<String, String> datas) {
+        Optional<UserEntity> userEntityWrapper = userRepository.findById(uid);
+        UserEntity userEntity = userEntityWrapper.get();
+
+        userEntity.setUser_id(datas.get("user_id"));
+        userEntity.setUser_email(datas.get("user_email"));
+        userEntity.setUser_nm(datas.get("user_nm"));
+        userEntity.setUser_rank(datas.get("user_rank"));
+        userEntity.setCorp_cd(datas.get("corp_cd"));
+        userEntity.setCorp_nm(datas.get("corp_nm"));
+        userEntity.setOrg_cd(datas.get("org_cd"));
+        userEntity.setOrg_nm(datas.get("org_nm"));
+        userEntity.setLevel_gp(datas.get("level_gp"));
+        userEntity.setLevel_lv(datas.get("level_lv"));
+
+        userRepository.save(userEntity);
+
+    }
+
+    @Transactional
+    public UserDto getUser(Long uid) {
+        Optional<UserEntity> userEntityWrapper = userRepository.findById(uid);
+        UserEntity userEntity = userEntityWrapper.get();
+
+        UserDto userDTO = UserDto.builder()
+                .uid(userEntity.getUid())
+                .user_id(userEntity.getUser_id())
+                .user_email(userEntity.getUser_email())
+                .user_nm(userEntity.getUser_nm())
+                .user_rank(userEntity.getUser_rank())
+                .corp_cd(userEntity.getCorp_cd())
+                .corp_nm(userEntity.getCorp_nm())
+                .org_nm(userEntity.getOrg_nm())
+                .org_cd(userEntity.getOrg_cd())
+                .level_gp(userEntity.getLevel_gp())
+                .level_lv(userEntity.getLevel_lv())
+                .build();
+
+        return userDTO;
+    }
+
+    @Transactional
+    public void deleteUser(Long uid) {
+        userRepository.deleteById(uid);
     }
 
 }
