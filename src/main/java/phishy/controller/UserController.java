5e1469abbf532e1d13f45c3ac8c76eee91adc703
jyoghh <4294967296z@gmail.com>
2021-07-15 14:22:17 +0900

    20210715

--- src/main/java/phishy/controller/UserController.java
+++ src/main/java/phishy/controller/UserController.java
@@ -1,4 +1,97 @@
 package phishy.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import phishy.dto.UserDto;
+import phishy.service.UserService;
+import phishy.util.ExcelRead;
+import phishy.util.ExcelReadOption;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
 public class UserController {
+
+    private UserService userService;
+
+    @RequestMapping(value = "/registerUser.do", method = RequestMethod.GET)
+    public String register(UserDto userDto,
+                           @RequestParam("user_id") String user_id,
+                           @RequestParam("user_email") String user_email,
+                           @RequestParam("user_nm") String user_nm,
+                           @RequestParam("user_pwd") String user_pwd,
+                           @RequestParam("user_rank") String user_rank,
+                           @RequestParam("corp_cd") String corp_cd,
+                           @RequestParam("corp_nm") String corp_nm,
+                           @RequestParam("org_cd") String org_cd,
+                           @RequestParam("org_nm") String org_nm,
+                           @RequestParam("level_gp") String level_gp,
+                           @RequestParam("level_lv") String level_lv) {
+        Map<String, String> list = new HashMap<String, String>();
+        list.put("user_id",user_id);
+        list.put("user_email",user_email);
+        list.put("user_nm",user_nm);
+        list.put("user_pwd",user_pwd);
+        list.put("user_rank",user_rank);
+        list.put("corp_cd",corp_cd);
+        list.put("corp_nm",corp_nm);
+        list.put("org_cd",org_cd);
+        list.put("org_nm",org_nm);
+        list.put("level_gp",level_gp);
+        list.put("level_lv",level_lv);
+        userService.registerUser(list);
+        return "redirect:/register?done=1";
+
+    }
+
+    @RequestMapping(value = "/excelUpload.do", method = RequestMethod.POST)
+    public @ResponseBody
+    Object excelUploadAjax(MultipartHttpServletRequest request)  throws Exception{
+        MultipartFile excelFile =request.getFile("excelFile");
+        System.out.println("엑셀 파일 업로드 컨트롤러");
+        if(excelFile==null || excelFile.isEmpty()){
+            throw new RuntimeException("엑셀파일을 선택 해 주세요.");
+        }
+
+        File destFile = new File("D:\\"+excelFile.getOriginalFilename());
+        try{
+            excelFile.transferTo(destFile);
+        }catch(IllegalStateException | IOException e){
+            throw new RuntimeException(e.getMessage(),e);
+        }
+
+        ExcelReadOption excelReadOption = new ExcelReadOption();
+        excelReadOption.setFilePath(destFile.getAbsolutePath());
+        excelReadOption.setOutputColumns("B","C","D","E","F","G","H","I");
+        excelReadOption.setStartRow(2);
+
+        List<Map<String, String>> excelContent = ExcelRead.read(excelReadOption);
+        List<Map<String, String>> totallist = new ArrayList<Map<String, String>>();
+        Map<String, String> map = null;
+
+        for(int rpt = 0; rpt < excelContent.size(); rpt++) {
+            map = new HashMap<String, String>();
+            map.put("email",excelContent.get(rpt).get("B"));
+            map.put("name",excelContent.get(rpt).get("C"));
+            map.put("corp",excelContent.get(rpt).get("D"));
+            map.put("dept",excelContent.get(rpt).get("E"));
+            map.put("ranks",excelContent.get(rpt).get("F"));
+            map.put("codes",excelContent.get(rpt).get("G"));
+            map.put("status",excelContent.get(rpt).get("H"));
+            map.put("i_group",excelContent.get(rpt).get("I"));
+            totallist.add(map);
+        }
+        userService.insertMultiUsers(totallist);
+
+        Object result = totallist;
+        return result;
+    }
+
 }
--- src/main/java/phishy/service/UserService.java
+++ src/main/java/phishy/service/UserService.java
@@ -8,6 +8,7 @@ import phishy.domain.Repository.UserRepository;
 import phishy.dto.UserDto;
 
 import javax.transaction.Transactional;
+import java.util.List;
 import java.util.Map;
 
 @AllArgsConstructor
@@ -21,18 +22,43 @@ public class UserService {
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         UserDto userDto = new UserDto();
         UserEntity userEntity = userDto.toEntity();
-        String status = datas.get("dept");
-        userEntity.setUser_id(datas.get("email"));
-        userEntity.setUser_pwd(passwordEncoder.encode(datas.get("pwd")));
-        userEntity.setUser_email(datas.get("email"));
-        userEntity.setUser_rank(datas.get("ranks"));
-        userEntity.setUser_nm(datas.get("name"));
+
+        userEntity.setUser_id(datas.get("user_id"));
+        userEntity.setUser_pwd(passwordEncoder.encode(datas.get("user_pwd")));
+        userEntity.setUser_email(datas.get("user_email"));
+        userEntity.setUser_nm(datas.get("user_nm"));
+        userEntity.setUser_rank(datas.get("user_rank"));
         userEntity.setCorp_cd(datas.get("corp_cd"));
         userEntity.setCorp_nm(datas.get("corp_nm"));
         userEntity.setOrg_cd(datas.get("org_cd"));
         userEntity.setOrg_nm(datas.get("org_nm"));
         userEntity.setLevel_gp(datas.get("level_gp"));
         userEntity.setLevel_lv(datas.get("level_lv"));
+
         userRepository.save(userEntity).getUid();
     }
+
+    @Transactional
+    public void insertMultiUsers(List<Map<String, String>> datas) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        UserDto userDto = new UserDto();
+
+        for(int idx = 0; idx < datas.size(); idx++) {
+            UserEntity userEntity = userDto.toEntity();
+            userEntity.setUser_id(datas.get(idx).get("user_id"));
+            userEntity.setUser_pwd(passwordEncoder.encode(datas.get(idx).get("user_pwd")));
+            userEntity.setUser_email(datas.get(idx).get("user_email"));
+            userEntity.setUser_rank(datas.get(idx).get("user_nm"));
+            userEntity.setUser_nm(datas.get(idx).get("user_rank"));
+            userEntity.setCorp_cd(datas.get(idx).get("corp_cd"));
+            userEntity.setCorp_nm(datas.get(idx).get("corp_nm"));
+            userEntity.setOrg_cd(datas.get(idx).get("org_cd"));
+            userEntity.setOrg_nm(datas.get(idx).get("org_nm"));
+            userEntity.setLevel_gp(datas.get(idx).get("level_gp"));
+            userEntity.setLevel_lv(datas.get(idx).get("level_lv"));
+
+            userRepository.save(userEntity).getUid();
+        }
+    }
+
 }
